4426e0497386dfe6a34aaea96e9278877ab5b647
MinhHieu <minhhieu77886@gmail.com>
2023-09-09 06:17:05 +0700

    background

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,14 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  var swiper = new Swiper(".background-container", {
+    direction: "vertical",
+    loop: true,
+    effect: "fade",
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
