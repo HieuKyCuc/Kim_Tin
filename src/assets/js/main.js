152022798308cd987542b08a2258b053abd6661b
MinhHieu <minhhieu77886@gmail.com>
2023-09-14 12:09:19 +0700

    info and number

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
   var swiper = new Swiper(".background-container", {
     direction: "vertical",
-    loop: true,
+    // loop: true,
     effect: "fade",
     autoplay: {
       delay: 3000,
     },
+    speed: 500,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
   });
+});
+
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() > 0) {
+    $(".header").addClass("opaque");
+  } else {
+    $(".header").removeClass("opaque");
+  }
 });
